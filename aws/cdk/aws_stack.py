cd1d833f96dd191d2db8a01042bcf32249f45934
Jared "Jay" Klein <j4klein95@gmail.com>
2022-07-14 20:37:24 -0400

    this didn't work and i am crying

--- aws/cdk/aws_stack.py
+++ aws/cdk/aws_stack.py
@@ -1,6 +1,5 @@
 import aws_cdk
 from aws_cdk import (
-    # Duration,
     Stack,
     aws_kms as kms,
     aws_s3 as s3,
@@ -12,14 +11,18 @@ from constructs import Construct
 
 class AwsStack(Stack):
 
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+    def __init__(self,
+                 scope: Construct,
+                 construct_id: str,
+                 bucket_name: str,
+                 **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
         encryption_key = kms.Key(self, 'Key', enable_key_rotation=True)
 
         # fix cdk.json to have the region
         bucket = s3.Bucket(self, 'jk-app-cloud-stats-bucket-us-east-1', encryption_key=encryption_key,
-                           removal_policy=RemovalPolicy.DESTROY)
+                           removal_policy=RemovalPolicy.DESTROY, bucket_name=bucket_name)
 
         # lambda to run the scraper
         lambda_.DockerImageFunction(self,
@@ -28,5 +31,6 @@ class AwsStack(Stack):
                                     function_name='predicting-scores-hcky-scraping-lambda',
                                     environment={"bucket": bucket.bucket_name},
                                     timeout=aws_cdk.Duration.seconds(300),
-                                    memory_size=256
+                                    memory_size=5000,
+                                    ephemeral_storage_size=aws_cdk.Size.mebibytes(512)
                                     )
--- engineering/lambda/app.py
+++ engineering/lambda/app.py
@@ -5,6 +5,7 @@ import boto3
 from io import StringIO
 
 from selenium import webdriver
+from selenium.webdriver.firefox.service import Service
 from selenium.webdriver import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -13,10 +14,19 @@ from selenium.webdriver.firefox.options import Options
 
 import pandas as pd
 
+executable_path = '/opt/geckodriver'
+
+service = Service(executable_path=executable_path, port=0, log_path=os.path.devnull)
+
 options = Options()
 options.headless = True
 options.add_argument("--no-sandbox")
-driver = webdriver.Firefox(options=options, executable_path='/opt/geckodriver', log_path='/dev/null')
+options.add_argument("--single-process")
+options.add_argument("--disable-dev-shm-usage")
+driver = webdriver.Firefox(options=options,
+                           executable_path=executable_path,
+                           service_log_path=os.path.devnull,
+                           service=service)
 
 
 def lambda_handler(event, context):
@@ -48,26 +58,25 @@ def lambda_handler(event, context):
     # Write scores to s3
     csv_buffer = StringIO()
     df.to_csv(csv_buffer)
+    # df.to_csv("scores")
 
     s3_resource = boto3.resource('s3')
     s3_resource.Object(os.environ['bucket'], 'scores.csv').put(Body=csv_buffer.getvalue())
-
-    selection_bar = driver.find_element(By.CSS_SELECTOR, "li.full:nth-child(6) > a:nth-child(1)")
-    actions.move_to_element(selection_bar).perform()
-
-    WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.CSS_SELECTOR,
-                                                                      "li.full:nth-child(6) > div:nth-child(2) > "
-                                                                      "ul:nth-child(1) > li:nth-child(2) > a:nth-child("
-                                                                      "1)"))).click()
-
-    advanced_stats_url = driver.current_url
-    as_df = pd.read_html(advanced_stats_url, index_col=0, header=1)[0]
-
-    as_df = as_df.drop_duplicates(subset=['Player'], keep='first', ignore_index=True)
-
-    as_df.to_csv(csv_buffer)
-
-    s3_resource = boto3.resource('s3')
-    s3_resource.Object(os.environ['bucket'], 'advanced_stats.csv').put(Body=csv_buffer.getvalue())
+    #
+    # selection_bar = driver.find_element(By.CSS_SELECTOR, "li.full:nth-child(6) > a:nth-child(1)")
+    # actions.move_to_element(selection_bar).perform()
+    #
+    # WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "li.full:nth-child(6) >
+    # div:nth-child(2) > " "ul:nth-child(1) > li:nth-child(2) > a:nth-child(" "1)"))).click()
+    #
+    # advanced_stats_url = driver.current_url
+    # as_df = pd.read_html(advanced_stats_url, index_col=0, header=1)[0]
+    #
+    # as_df = as_df.drop_duplicates(subset=['Player'], keep='first', ignore_index=True)
+    #
+    # as_df.to_csv(csv_buffer)
+    #
+    # s3_resource = boto3.resource('s3')
+    # s3_resource.Object(os.environ['bucket'], 'advanced_stats.csv').put(Body=csv_buffer.getvalue())
 
     driver.quit()
